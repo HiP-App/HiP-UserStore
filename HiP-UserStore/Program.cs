309fb46cf05ebe2bc0be7a2e3dfc79d6a3675428
Sven Erik Vinkemeier <svenvinkemeier@live.de>
2017-09-08 15:14:44 +0200

    Basic event sourcing setup

---
+++ HiP-UserStore/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+using Microsoft.AspNetCore.Hosting;
+
+namespace PaderbornUniversity.SILab.Hip.DataStore
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseUrls("http://*:5000")
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .UseApplicationInsights()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
---
+++ HiP-UserStore/Startup.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using PaderbornUniversity.SILab.Hip.EventSourcing;
+using PaderbornUniversity.SILab.Hip.UserStore.Core;
+using PaderbornUniversity.SILab.Hip.UserStore.Utility;
+using PaderbornUniversity.SILab.Hip.Webservice;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace PaderbornUniversity.SILab.Hip.DataStore
+{
+    public class Startup
+    {
+        private const string Version = "v1";
+        private const string Name = "HiP User Store API";
+
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Register the Swagger generator
+            services.AddSwaggerGen(c =>
+            {
+                // Define a Swagger document
+                c.SwaggerDoc("v1", new Info { Title = Name, Version = Version });
+                c.OperationFilter<SwaggerOperationFilter>();
+                c.DescribeAllEnumsAsStrings();
+            });
+
+            services
+                .Configure<EndpointConfig>(Configuration.GetSection("Endpoints"))
+                .Configure<CorsConfig>(Configuration);
+
+            services
+                .AddSingleton<EventStoreClient>()
+                .AddSingleton<InMemoryCache>();
+
+            services.AddCors();
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory,
+            IOptions<CorsConfig> corsConfig, IOptions<EndpointConfig> endpointConfig)
+        {
+            loggerFactory
+                .AddConsole(Configuration.GetSection("Logging"))
+                .AddDebug();
+
+            // EventStoreClient should start up immediately (not only when injected into a controller or
+            // something), so we manually request an instance here
+            app.ApplicationServices.GetService<EventStoreClient>();
+
+            // Use CORS (important: must be before app.UseMvc())
+            app.UseCors(builder =>
+            {
+                var corsEnvConf = corsConfig.Value.Cors[env.EnvironmentName];
+                builder
+                    .WithOrigins(corsEnvConf.Origins)
+                    .WithMethods(corsEnvConf.Methods)
+                    .WithHeaders(corsEnvConf.Headers)
+                    .WithExposedHeaders(corsEnvConf.ExposedHeaders);
+            });
+
+            app.UseMvc();
+
+            // Swagger / Swashbuckle configuration:
+            // Enable middleware to serve generated Swagger as a JSON endpoint
+            app.UseSwagger(c =>
+            {
+                c.PreSerializeFilters.Add((swaggerDoc, httpReq) => swaggerDoc.Host = httpReq.Host.Value);
+            });
+
+            // Configure SwaggerUI endpoint
+            app.UseSwaggerUI(c =>
+            {
+                var swaggerJsonUrl = string.IsNullOrEmpty(endpointConfig.Value.SwaggerEndpoint)
+                    ? $"/swagger/{Version}/swagger.json"
+                    : endpointConfig.Value.SwaggerEndpoint;
+
+                c.SwaggerEndpoint(swaggerJsonUrl, $"{Name} {Version}");
+            });
+        }
+    }
+}
