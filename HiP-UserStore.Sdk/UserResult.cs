9528bc7127c7819612201e3b28bff9f310ed95b2
Sven Vinkemeier <svenvinkemeier@live.de>
2017-10-31 18:39:48 +0100

    Added property UserResult.FullName in SDK, collections in SDK are now IReadOnlyLists, bumped version to v0.2.0

---
+++ HiP-UserStore.Sdk/UserResult.cs
@@ -0,0 +1,7 @@
+﻿namespace PaderbornUniversity.SILab.Hip.UserStore
+{
+    public partial class UserResult
+    {
+        public string FullName => (FirstName == null && LastName == null) ? null : FirstName + ' ' + LastName;
+    }
+}
