0f13ed5b65cb608760923f87fe8da8c9380f9247
Sven Vinkemeier <svenvinkemeier@live.de>
2017-11-25 16:30:49 +0100

    Removed unused using

--- HiP-UserStore/Startup.cs
+++ HiP-UserStore/Startup.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
