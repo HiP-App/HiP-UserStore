d5f3e31f8bbd6ef1a2660c96571c865bf8c5ca06
Sven Vinkemeier <svenvinkemeier@live.de>
2017-11-25 12:27:34 +0100

    Bugfix

--- HiP-UserStore/Core/CacheDatabaseManager.cs
+++ HiP-UserStore/Core/CacheDatabaseManager.cs
@@ -52,9 +52,9 @@ namespace PaderbornUniversity.SILab.Hip.UserStore.Core
                         Id = e.Id,
                         UserId = e.UserId,
                         Timestamp = e.Timestamp,
-                        Email = e.Properties.Email,
-                        FirstName = e.Properties.FirstName,
-                        LastName = e.Properties.LastName
+                        Email = e.Properties?.Email,
+                        FirstName = e.Properties?.FirstName,
+                        LastName = e.Properties?.LastName
                     };
                     _db.GetCollection<User>(ResourceType.User.Name).InsertOne(newUser);
                     break;
