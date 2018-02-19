dced35beb6d5ecaa5a82e3f3948ee4d447c0d5f7
Michél Burkhardt <mburkhardt@live.de>
2018-02-19 10:41:57 +0100

    Fixed folder name

--- HIP-UserStore.Model/Rest/Actions/ExhibitVisitedActionResult.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PaderbornUniversity.SILab.Hip.UserStore.Model.Rest.Actions
-{
-    class ExhibitVisitedActionResult : ActionResult
-    {
-        public ExhibitVisitedActionResult(Entity.Action action) : base(action)
-        {
-        }
-    }
-}
