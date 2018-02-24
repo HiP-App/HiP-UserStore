9cac0a107e562d6e32a498f59b96547742f5d870
Michél Burkhardt <mburkhardt@live.de>
2018-02-24 11:47:17 +0100

    Made use of inheritance support from NSwag, removed unnecesarry endpoint

--- HiP-UserStore/Controllers/ActionsController.cs
+++ HiP-UserStore/Controllers/ActionsController.cs
@@ -15,46 +15,46 @@ using PaderbornUniversity.SILab.Hip.EventSourcing;
 namespace PaderbornUniversity.SILab.Hip.UserStore.Controllers
 {
 
-        [Authorize]
-        [Route("api/[controller]")]
-        public class ActionsController : Controller
-        {
-            private readonly CacheDatabaseManager _db;
+    [Authorize]
+    [Route("api/[controller]")]
+    public class ActionsController : Controller
+    {
+        private readonly CacheDatabaseManager _db;
 
-            public ActionsController(CacheDatabaseManager db)
+        public ActionsController(CacheDatabaseManager db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(AllItemsResult<ActionResult>), 200)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(404)]
+        public IActionResult GetAllActions(string actionType = null)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            if (actionType != null)
             {
-                _db = db;
+                var listOfTypes = typeof(ActionTypes).GetProperties(BindingFlags.Public | BindingFlags.Static)
+                                                 .Where(f => f.PropertyType == typeof(ResourceType))
+                                                 .ToDictionary(f => f.Name,
+                                                               f => (ResourceType)f.GetValue(null));
+
+                if (!listOfTypes.Any(x => x.Value.Name == actionType))
+                    return NotFound(new { Message = $"Action type '{actionType}' is not supported" });
             }
 
-            [HttpGet]
-            [ProducesResponseType(typeof(AllItemsResult<ActionResult>), 200)]
-            [ProducesResponseType(400)]
-            [ProducesResponseType(404)]
-        public IActionResult GetAllActions(string actionType = null)
-            {
-                  if (!ModelState.IsValid)
-                   return BadRequest(ModelState);
-
-                  if (actionType != null)
-                  {
-                      var listOfTypes =  typeof(ActionTypes).GetProperties(BindingFlags.Public | BindingFlags.Static)
-                                                       .Where(f => f.PropertyType == typeof(ResourceType))
-                                                       .ToDictionary(f => f.Name,
-                                                                     f => (ResourceType)f.GetValue(null));
-                
-                      if (!listOfTypes.Any(x => x.Value.Name == actionType))
-                          return NotFound(new { Message = $"Action type '{actionType}' is not supported" });
-                  }
-
-                var query = _db.Database.GetCollection<Action>(ResourceTypes.Action.Name).AsQueryable();
-                var userId = User.Identity.GetUserIdentity();
-                var result = query.Where(x => x.UserId == userId)
-                                  .ToList()
-                                  .AsQueryable()
-                                  .FilterIf(actionType != null, x => x.TypeName == actionType)
-                                  .Select(x => x.CreateActionResult())
-                                  .ToList();
-                return Ok(new AllItemsResult<ActionResult>() { Total = result.Count, Items = result });
-            }         
-    }    
+            var query = _db.Database.GetCollection<Action>(ResourceTypes.Action.Name).AsQueryable();
+            var userId = User.Identity.GetUserIdentity();
+            var result = query.Where(x => x.UserId == userId)
+                              .ToList()
+                              .AsQueryable()
+                              .FilterIf(actionType != null, x => x.TypeName == actionType)
+                              .Select(x => x.CreateActionResult())
+                              .ToList();
+            return Ok(new AllItemsResult<ActionResult>() { Total = result.Count, Items = result });
+        }
+    }
 }
