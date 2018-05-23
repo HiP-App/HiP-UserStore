47c51cf74a7f7729d7305c70f14b03db9d3eb1f8
Michél Burkhardt <mburkhardt@live.de>
2018-05-23 14:55:46 +0200

    Added filtering option for the beginning of an email (#47)

--- HiP-UserStore.Model/Rest/UserQueryArgs.cs
+++ HiP-UserStore.Model/Rest/UserQueryArgs.cs
@@ -6,5 +6,10 @@
         /// Restricts the response result to users having the specified role.
         /// </summary>
         public string Role { get; set; }
+
+        /// <summary>
+        /// Restricts the response to users having an email that begins with the specified string
+        /// </summary>
+        public string EmailBeginning { get; set; }
     }
 }
