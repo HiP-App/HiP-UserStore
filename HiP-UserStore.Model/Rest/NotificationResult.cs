b0a29cddb30f983c21fc6d7316400bc41a6a2332
Michél Burkhardt <mburkhardt@live.de>
2018-07-08 18:14:55 +0200

    Added notifications to UserStore (iss-hipcms-1044) (#54)
    * WIP
    
    * Removed HiP-NotificationLib since it is no longer needed, moved all classes to Model project
    
    * Fixed R# warnings

---
+++ HiP-UserStore.Model/Rest/NotificationResult.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using NJsonSchema.Converters;
+using PaderbornUniversity.SILab.Hip.UserStore.Model.Entity;
+using PaderbornUniversity.SILab.Hip.UserStore.Model.Rest.Notifications;
+using System;
+using System.Runtime.Serialization;
+
+namespace PaderbornUniversity.SILab.Hip.UserStore.Model.Rest
+{
+    /// <summary>
+    /// This class functions as a base class for a specifc notificatoin result class and contains basic properties that every notification has 
+    /// </summary>
+    [JsonConverter(typeof(JsonInheritanceConverter), "discriminator")]
+    [KnownType(typeof(ReviewNotificationResult))]
+    public abstract class NotificationResult
+    {
+        public int Id { get; set; }
+
+        public string Type { get; set; }
+
+        public bool IsRead { get; set; }
+        public string Text { get; set; }
+        public DateTimeOffset Timestamp { get; set; }
+
+        public NotificationResult(NotificationBase notification)
+        {
+            Id = notification.Id;
+            Type = notification.Type.ToString();
+            IsRead = notification.IsRead;
+            Text = notification.Text;
+            Timestamp = notification.Timestamp;
+        }
+    }
+}
---
+++ HiP-UserStore/Controllers/NotificationBaseController.cs
@@ -0,0 +1,53 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using PaderbornUniversity.SILab.Hip.EventSourcing;
+using PaderbornUniversity.SILab.Hip.EventSourcing.EventStoreLlp;
+using PaderbornUniversity.SILab.Hip.UserStore.Core;
+using PaderbornUniversity.SILab.Hip.UserStore.Model;
+using PaderbornUniversity.SILab.Hip.UserStore.Model.Rest;
+using PaderbornUniversity.SILab.Hip.UserStore.Utility;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace PaderbornUniversity.SILab.Hip.UserStore.Controllers
+{
+    [Route("api/Notifications/[controller]")]
+    [Authorize]
+    public abstract class NotificationBaseController<TArgs> : Controller where TArgs : NotificationBaseArgs, new()
+    {
+        private EventStoreService _eventStore;
+        private EntityIndex _entityIndex;
+
+        /// <summary>
+        /// This resource type is used for creating the events for the EventStore
+        /// </summary>
+        protected abstract ResourceType ResourceType { get; }
+
+        public NotificationBaseController(EventStoreService eventStore, InMemoryCache cache)
+        {
+            _eventStore = eventStore;
+            _entityIndex = cache.Index<EntityIndex>();
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(int), 201)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(403)]
+        public async Task<IActionResult> CreateNotification([FromBody]TArgs args)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            if (User.Identity.GetUserIdentity() == null)
+                return Forbid();
+
+            //the BaseResourceType is used in this case since we want to have following id's even for different types
+            var id = _entityIndex.NextId(ResourceTypes.Notification);
+            await EntityManager.CreateEntityAsync(_eventStore, args, ResourceType, id, User.Identity.GetUserIdentity());
+            return Created($"{Request.Scheme}://{Request.Host}/api/Notification/{id}", id);
+        }
+
+
+    }
+}
