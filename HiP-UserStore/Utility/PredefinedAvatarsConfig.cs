f9c733441cd9b092ab9acd59ff1b1f35fd80e958
JohannesH90 <38187779+JohannesH90@users.noreply.github.com>
2018-07-30 10:35:39 +0200

    Predefined Avatars added.

---
+++ HiP-UserStore/Utility/PredefinedAvatarsConfig.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace PaderbornUniversity.SILab.Hip.UserStore.Utility
+{
+    public class PredefinedAvatarsConfig
+    {
+        /// <summary>
+        /// Path to the directory where profile pictures are stored.
+        /// Default value: "Avatar"
+        /// </summary>
+        public string Path { get; set; } = "Avatar";
+
+        /// <summary>
+        /// A list of supported file extensions (without leading dot) for profile pictures.
+        /// </summary>
+        public List<string> SupportedFormats { get; set; }
+    }
+}
